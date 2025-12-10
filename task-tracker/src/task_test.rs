fb754fe2b00b9c32724b7b9f290c7114a923e0f4
Ocki Bagus Pratama <ocki.bagus.p@gmail.com>
2025-12-10 09:17:09 +0700

    feat: add task@add should fail

--- task-tracker/src/task_test.rs
+++ task-tracker/src/task_test.rs
@@ -1,9 +1,12 @@
-use crate::task::TaskManager;
+use std::fs;
+use std::io::{Error, ErrorKind};
+
+use crate::files;
 use crate::task::TaskManagerTrait;
-use crate::task::{MockTaskManagerTrait, MockTaskTrait, Task, TaskTrait};
-use crate::task_test;
+use crate::task::{MockTaskManagerTrait, MockTaskTrait, TaskTrait};
+// use mockall::mock;
+use crate::task::TaskManager;
 use chrono::DateTime;
-use chrono_tz::Africa::Tripoli;
 use mockall::predicate::*;
 
 /*
@@ -12,73 +15,145 @@ use mockall::predicate::*;
 #[test]
 fn test_task_trait_fail() {
     let mut mock = MockTaskTrait::new();
-    mock.expect_is_validation().returning(|| false);
-    assert_eq!(mock.is_validation(), false);
+    let invalid_input = ErrorKind::InvalidInput;
+
+    mock.expect_is_validation()
+        .returning(move || Err(Error::new(invalid_input, "error")));
+    assert_eq!(mock.is_validation().is_err(), true);
 }
 
 #[test]
 fn test_task_trait_success() {
-    let mut mock = MockTaskTrait::new();
-    mock.expect_is_validation().returning(|| true);
-    assert_eq!(mock.is_validation(), true);
+    let mut mock = MockTaskTrait::default();
+    mock.expect_is_validation().returning(|| Ok(()));
+    assert_eq!(mock.is_validation().unwrap(), ());
 }
 
 /*
     TaskManager
 */
+// mock! {
+//     pub TaskManager {
+//         pub file: crate::files::File,
+//         pub list: Vec<Task>,
+//         pub next_id: i32,
+//     }
+
+//     impl TaskManagerTrait for TaskManager {
+//         fn new(file_name: &'static str) -> Self;
+//         fn get_next_id(&self) -> i32;
+//         fn list(&self) -> Vec<crate::task::Task>;
+//         fn add(&mut self, input: &str) -> Result<crate::task::Task, Error>;
+//     }
+// }
 #[test]
-fn test_mock_list_fail() {
-    let mut t = <TaskManager as TaskManagerTrait>::new("test-task-cli.json");
-    let _ = t.list();
-    // test
-
-    // let task = Task {
-    //     id: 1,
-    //     description: String::from("buy milk"),
-    //     status: TaskStatus::Todo,
-    //     created_at: DateTime::parse_from_str(
-    //         "2025-04-10 10:10:10.000000 +07:00",
-    //         "%Y-%m-%d %H:%M:%S%.6f %z",
-    //     )
-    //     .unwrap()
-    //     .into(),
-    //     updated_at: DateTime::parse_from_str(
-    //         "2025-04-10 10:10:10.000000 +07:00",
-    //         "%Y-%m-%d %H:%M:%S%.6f %z",
-    //     )
-    //     .unwrap()
-    //     .into(),
-    // };
-
-    // let m = task.clone();
-
-    // let mut mock = MockTaskManagerTrait::new("test-task-cli.log");
-    // mock.expect_list()
-    //     .times(1)
-    //     .returning(move || vec![task.clone()]);
-    // assert_eq!(mock.list(), vec![m]);
+fn test_task_manager_trait_new() {
+    let file_name = "test-task-cli.json";
+    let m = TaskManager::new(file_name);
+    assert!(Some(m.file).is_some());
+    assert_eq!(m.list, vec![]);
+    fs::remove_file(file_name).unwrap();
 }
 
-// #[test]
-// fn test_add_success() {
-//     let created_at = DateTime::parse_from_str("1970-01-01 00:00:00 +00:00", "%Y-%m-%d %H:%M:%S %z")
-//         .unwrap()
-//         .into();
+#[test]
+fn test_mock_list() {
+    // // Setup Mock for new, so no function
+    // MockTaskManagerTrait::expect_new()
+    //     .with(mockall::predicate::eq("test-task-cli"))
+    //     .returning(|name| MockTaskManagerTrait {
+    //         name,
+    //         ..Default::default()
+    //     });
 
-//     let _add_task = Task {
-//         id: 2,
-//         description: "test".to_string(),
-//         status: TaskStatus::Todo,
-//         created_at: created_at,
-//         updated_at: created_at,
-//     };
+    // let mut mock = MockTaskManagerTrait::new("test-task-cli");
 
-//     let mut mock = MockTaskManagerTrait::new("test.json");
-//     mock.expect_add()
-//         .with(eq("test"))
-//         .returning(|_| Ok(_add_task.clone()));
-//     assert_eq!(mock.add("test"), Ok(_add_task));
-// }
+    /*
+     * empty list
+     */
+    let mut mock = MockTaskManagerTrait::default();
+    mock.expect_list().times(1).return_once(|| vec![]);
+    assert_eq!(mock.list(), vec![]);
+
+    /*
+     * one task in list
+     */
+    // test with one task
+    let task = files::tests::setup_task(1, "test one");
+    let task_one = task.clone();
+
+    mock.expect_list()
+        .times(1)
+        .return_once(move || vec![task.clone()]);
+    assert_eq!(mock.list(), vec![task_one]);
+}
+
+#[test]
+fn test_mock_add_fail() {
+    let created_at = DateTime::parse_from_str("1970-01-01 00:00:00 +00:00", "%Y-%m-%d %H:%M:%S %z")
+        .unwrap()
+        .into();
+
+    // negative id
+    let mut _add_task_fail = crate::task::Task {
+        id: -1,
+        description: "test buy one".to_string(),
+        status: crate::task::VALID_STATUSES[0].to_string(),
+        created_at: created_at,
+        updated_at: created_at,
+    };
+
+    let mut mock = MockTaskManagerTrait::default();
+
+    mock.expect_add().with(eq("test buy one")).returning(|_| {
+        Err(Error::new(
+            std::io::ErrorKind::InvalidInput,
+            "Error: [TaskTrait] `id` is negative",
+        ))
+    });
+    let result = mock.add("test buy one");
+    assert!(result.is_err());
+    let err = result.unwrap_err();
+    assert_eq!(err.kind(), std::io::ErrorKind::InvalidInput);
+    assert_eq!(err.to_string(), "Error: [TaskTrait] `id` is negative");
+
+    // is empty description
+    mock.expect_add().with(eq("")).returning(|_| {
+        Err(Error::new(
+            std::io::ErrorKind::InvalidInput,
+            "Error: [TaskTrait] `description` is empty or too long",
+        ))
+    });
+    let result = mock.add("");
+    assert!(result.is_err());
+    let err = result.unwrap_err();
+    assert_eq!(err.kind(), std::io::ErrorKind::InvalidInput);
+    assert_eq!(
+        err.to_string(),
+        "Error: [TaskTrait] `description` is empty or too long"
+    );
+
+    // is too long description
+    let long_description = "a".repeat(30);
+    mock.expect_add()
+        .with(eq(long_description.clone()))
+        .returning(|_| {
+            Err(Error::new(
+                std::io::ErrorKind::InvalidInput,
+                "Error: [TaskTrait] `description` is empty or too long",
+            ))
+        });
+    let result = mock.add(&long_description);
+    assert!(result.is_err());
+    let err = result.unwrap_err();
+    assert_eq!(err.kind(), std::io::ErrorKind::InvalidInput);
+    assert_eq!(
+        err.to_string(),
+        "Error: [TaskTrait] `description` is empty or too long"
+    );
+
+    // is invalid status
+    // Note: Since status is not passed in add function, this case is just for demonstration or is never used.
+}
 
 // #[test]
 // fn test_mock_add_success() {
