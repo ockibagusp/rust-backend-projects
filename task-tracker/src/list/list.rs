a57c407bbe51b747e9b2c76220297b88decfc2bc
Ocki Bagus Pratama <ocki.bagus.p@gmail.com>
2025-12-22 17:54:19 +0700

    feat: add list@{new index} should fail and success

--- task-tracker/src/list/list.rs
+++ task-tracker/src/list/list.rs
@@ -1,6 +1,27 @@
-pub struct List;
+use mockall::automock;
 
-impl List {
-    pub fn new() {}
-    pub fn index() {}
+use crate::task::task::Task;
+use crate::task::task_manager::{TaskManager, TaskManagerTrait};
+
+pub struct List {
+    pub task_manager: TaskManager,
+}
+
+#[automock]
+pub trait ListTrait {
+    fn new(file_name: &'static str) -> Self;
+    fn index(&self) -> Vec<Task>;
+}
+
+impl ListTrait for List {
+    fn new(file_name: &'static str) -> Self {
+        let _task_manager = TaskManager::new(file_name);
+        List {
+            task_manager: _task_manager,
+        }
+    }
+    // Get all task list
+    fn index(&self) -> Vec<Task> {
+        return self.task_manager.list();
+    }
 }
--- task-tracker/src/list/mod.rs
+++ task-tracker/src/list/mod.rs
@@ -1,1 +1,2 @@
 pub mod list;
+mod list_test;
