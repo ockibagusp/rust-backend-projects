a2dbb09ed53bccdd35d383c35a92f8c846a3a8ff
Ocki Bagus Pratama <ocki.bagus.p@gmail.com>
2025-12-30 23:33:39 +0700

    feat: adding task_mark@{new mark_in_progress} should success

--- task-tracker/src/task/task_mark_test.rs
+++ task-tracker/src/task/task_mark_test.rs
@@ -1,5 +1,6 @@
 use crate::task::task_mark::{MockTaskMarkTrait, TaskMark, TaskMarkTrait};
 
+use chrono::DateTime;
 use mockall::predicate::*;
 use std::io::Error;
 
@@ -38,4 +39,31 @@ fn test_mock_in_progress_should_fail() {
         err.to_string(),
         "error: Task is already in 'in-progress' status"
     );
+}
+
+#[test]
+fn test_mock_in_progress_should_success() {
+    let created_at = DateTime::parse_from_str("1970-01-01 00:00:00 +00:00", "%Y-%m-%d %H:%M:%S %z")
+        .unwrap()
+        .into();
+
+    const TASK_DESC: &str = "test buy one";
+    let task_to_update = crate::task::task::Task {
+        id: 1,
+        description: TASK_DESC.to_string(),
+        status: crate::task::task::VALID_STATUSES[0].to_string(),
+        created_at: created_at,
+        updated_at: created_at,
+    };
+
+    let mut mock = MockTaskMarkTrait::default();
+
+    mock.expect_mark_in_progress()
+        .with(eq(1))
+        .returning(move |_| Ok(task_to_update.clone()));
+    let result = mock.mark_in_progress(1);
+    assert!(result.is_ok());
+    let task = result.unwrap();
+    assert_eq!(task.id, 1);
+    assert_eq!(task.description, TASK_DESC.to_string());
 }
