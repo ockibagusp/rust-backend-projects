ab553cfb025e70699c146dc7e1325037b24a95aa
Ocki Bagus Pratama <ocki.bagus.p@gmail.com>
2025-12-30 23:06:11 +0700

    feat: adding task_mark@{new mark_in_progress} should success

--- task-tracker/src/task/mod.rs
+++ task-tracker/src/task/mod.rs
@@ -1,5 +1,6 @@
 pub mod task;
 pub mod task_manager;
 pub mod task_manager_test;
+pub mod task_mark;
 pub mod task_mark_test;
 pub mod task_test;
---
+++ task-tracker/src/task/task_mark.rs
@@ -0,0 +1,59 @@
+use crate::file::files::File;
+use crate::task::task::{Task, TaskTrait};
+use crate::task::task_manager::{TaskManager, TaskManagerTrait};
+use chrono::prelude::*;
+use mockall::*;
+use std::io::Error;
+
+fn error_invalid_input(message: &str) -> Error {
+    return Error::new(
+        std::io::ErrorKind::InvalidInput,
+        format!("error: {}", message),
+    );
+}
+
+fn error_not_found_input(message: &str) -> Error {
+    Error::new(std::io::ErrorKind::NotFound, format!("error: {}", message))
+}
+
+#[derive(PartialEq, Debug)]
+pub struct TaskMark {
+    pub task_manager: TaskManager,
+}
+
+#[automock]
+pub trait TaskMarkTrait {
+    fn new(file_name: &'static str) -> Self;
+    fn mark_in_progress(&self, id: i32) -> Result<Task, Error>;
+}
+
+impl TaskMarkTrait for TaskMark {
+    fn new(file_name: &'static str) -> Self {
+        let task_manager = TaskManager::new(file_name);
+
+        TaskMark {
+            task_manager: task_manager,
+        }
+    }
+
+    fn mark_in_progress(&self, id: i32) -> Result<Task, Error> {
+        let mut task_to_update = self
+            .task_manager
+            .list
+            .iter()
+            .find(|&task| task.id == id)
+            .ok_or_else(|| error_not_found_input("`id` is not found"))?
+            .clone();
+
+        if task_to_update.status == "in-progress" {
+            return Err(error_invalid_input(
+                "Task is already in 'in-progress' status",
+            ));
+        }
+
+        task_to_update.status = "in-progress".to_string();
+
+        let _ = &self.task_manager.update(id, &mut task_to_update);
+        Ok(task_to_update)
+    }
+}
