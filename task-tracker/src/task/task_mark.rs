4de0919bdc82d3ff902060724bdeae729eee7d13
Ocki Bagus Pratama <ocki.bagus.p@gmail.com>
2025-12-31 00:06:54 +0700

    feat: adding task_mark@{new mark_in_progress} should fail (true)

--- task-tracker/src/task/task_mark.rs
+++ task-tracker/src/task/task_mark.rs
@@ -25,6 +25,7 @@ pub struct TaskMark {
 pub trait TaskMarkTrait {
     fn new(file_name: &'static str) -> Self;
     fn mark_in_progress(&self, id: i32) -> Result<Task, Error>;
+    fn mark_done(&self, id: i32) -> Result<Task, Error>;
 }
 
 impl TaskMarkTrait for TaskMark {
@@ -46,14 +47,26 @@ impl TaskMarkTrait for TaskMark {
             .clone();
 
         if task_to_update.status == VALID_STATUSES[1] {
-            return Err(error_invalid_input(
-                "Task is already in 'in-progress' status",
-            ));
+            return Err(error_invalid_input("Task is already in 'done' status"));
         }
 
         task_to_update.status = VALID_STATUSES[1].to_string();
 
         let _ = &self.task_manager.update(id, &mut task_to_update);
         Ok(task_to_update)
+    }
+
+    fn mark_done(&self, id: i32) -> Result<Task, Error> {
+        let mut task_to_update = self
+            .task_manager
+            .list
+            .iter()
+            .find(|&task| task.id == id)
+            .ok_or_else(|| error_not_found_input("`id` is not found"))?
+            .clone();
+
+        if task_to_update.status == VALID_STATUSES[2] {
+            return Err(error_invalid_input("Task is already in 'done' status"));
+        }
     }
 }
