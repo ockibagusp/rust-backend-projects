33cdd9b1d72f98ad1837fc3fc6220d1724894caa
Ocki Bagus Pratama <ocki.bagus.p@gmail.com>
2026-03-10 22:00:11 +0700

    todos: all of task

--- task-tracker/src/task/task.rs
+++ task-tracker/src/task/task.rs
@@ -40,6 +40,27 @@ use serde::{Deserialize, Serialize};
 // }
 pub const VALID_STATUSES: [&str; 3] = ["todo", "in-progress", "done"];
 
+// TDD
+// ✅ ❔ ❌
+// 2.2. buatlah validasi untuk struktur data Task ✅
+// => 2.2. create the validation for the Task data structure
+// ------------------------------------------------
+// 1. pastikan field `id` harus memiliki nilai positif ✅
+// => 1. ensure that the `id` field should have a positive value
+// 2. pastikan field `description` harus memiliki:
+//      - minimal 2 karakter
+//      - maksimal 50 karakter ✅
+// => 2. ensure that the `description` field should have:
+//      - a minimum of 2 characters
+//      - a maximum of 50 characters
+// 3. pastikan field `status` hanya boleh memiliki nilai 'todo', 'in-progress', atau 'done' ✅
+// => 3. ensure that the `status` field should only have values 'todo', 'in-progress', or 'done'
+// 4. pastikan field `created_at` harus berisi waktu pertama kali dimasukkan ✅
+// => 4. ensure that the `created_at` field should contain the first time it was inserted
+// 5. pastikan field `updated_at` harus berisi waktu terakhir kali diubah ✅
+// => 5. ensure that the `updated_at` field should contain the last time it was updated
+// ------------------------------------------------
+
 // #[derive(PartialEq, Clone, Debug, SerializeDisplay, DeserializeDisplay)]
 #[derive(PartialEq, Clone, Debug, Serialize, Deserialize)]
 pub struct Task {
@@ -53,19 +74,20 @@ pub struct Task {
 
 // TDD
 // ✅ ❔ ❌
-// 2.2. buatlah validasi untuk struktur data Task ✅
-// => 2.2. create the validation for the Task data structure
+// 2.3. buatlah trait untuk validasi Task ✅
+// => 2.3. create a trait for Task validation
 // ------------------------------------------------
-// 1. pastikan field `id` harus memiliki nilai positif ✅
-// => 1. ensure that the `id` field should have a positive value
-// 2. pastikan field `description` tidak boleh:
-//      - minimal 2 karakter
-//      - maksimal 50 karakter ✅
-// => 2. ensure that the `description` field should have:
-//      - a minimum of 2 characters
-//      - a maximum of 50 characters
-// 3. pastikan field `status` hanya boleh memiliki nilai 'todo', 'in-progress', atau 'done' ✅
-// => 3. ensure that the `status` field should only have values 'todo', 'in-progress', or 'done'
+// 1. buat trait `TaskTrait` dengan method `is_validation` yang mengembalikan nil atau status kesalahan teks  ✅
+// => 1. create the `TaskTrait` trait with the `is_validation` method that returns nil or a text error status
+// 2. implementasikan trait `TaskTrait` untuk struct `Task` dengan:
+//      - validasi untuk field `description`sesuai dengan poin 2.2
+//      - validasi untuk field `id` harus tidak boleh negatif
+//      - validasi untuk field `status` harus memiliki nilai 'todo', 'in-progress', atau 'done'
+// => 2. implement the `TaskTrait` trait for the `Task` struct with:
+//      - validation for the `description` field according to point 2.2
+//      - validation for the `id` field should not be negative
+//      - validation for the `status` field should have values 'todo', 'in-progress', or 'done'
+// ------------------------------------------------
 #[automock]
 pub trait TaskTrait {
     fn is_validation(&self) -> Result<(), &'static str>;
@@ -74,19 +96,17 @@ pub trait TaskTrait {
 impl TaskTrait for Task {
     fn is_validation(&self) -> Result<(), &'static str> {
         if self.id.is_negative() {
-            return Err("`id` is negative");
+            return Err("ID is negative");
         }
         if self.description.trim().is_empty()
             || self.description.len() < 2
             || self.description.len() > 50
         {
-            return Err(
-                "`description` is empty or too short(min. 2 chars) or too long(max. 50 chars)",
-            );
+            return Err("DESCRIPTION is too short(min. 2 chars) or too long(max. 50 chars)");
         }
         let _valid_statuses = VALID_STATUSES;
         if !matches!(&self.status, _valid_statuses) {
-            return Err("`status` is invalid: \"todo\", \"in-progress\" or \"done\"");
+            return Err("STATUS is invalid: \"todo\", \"in-progress\" or \"done\"");
         }
         Ok(())
     }
